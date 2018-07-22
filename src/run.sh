1a1358d43c28328f0ef7f6ea7e4512fbfbf114d3
Andrew Perry <andrew.perry@cloudreach.com>
2018-07-22 16:51:41 +0100

    feat: add logging, error handling and use mktemp for temp files
    If the command fails then the script that was attempted will be
    placed in the working directory along with a log file containing
    the output from the script.

--- src/run.sh
+++ src/run.sh
@@ -7,15 +7,27 @@ function search_file {
   echo "searching $1"
   a=$(cat $file | jq --arg COMMAND $2 '.commands[] | select(.name == $COMMAND)')
   if [ "$a" != "" ]; then
+    CMD=$(echo $a | jq -r '.value')
     num=$(cat $file | jq '.env | length')
+    if [ "$num" != "0" ]; then
     for index in $( seq 1 ${num}); do
         env_json=$(cat $file | jq  --arg INDEX $((index-1)) '.env[$INDEX |tonumber]')
-        CMD="${CMD} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
+        ENV="${ENV} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
     done
+    fi
 
-    echo -e '#!/bin/bash\necho_baz() { echo "[$DEBUG]"; }\n echo_baz' > /tmp/a.txt
-    sh -c "${CMD} /tmp/a.txt"
-    # echo ${CMD}
+    TEMP_FILE=$(mktemp /tmp/run.XXXXXXXX)
+    LOG_FILE=$(mktemp /tmp/run.XXXXXXXX)
+    echo -e '#!/bin/bash\nrun() {\n '"${CMD}"'\n}\n run' > ${TEMP_FILE}
+    chmod +x ${TEMP_FILE}
+    sh -c "${ENV} ${TEMP_FILE}" | tee ${LOG_FILE}
+    if [ "$?" -ne "0" ]; then
+      mv ${TEMP_FILE} ${PWD}/run.failed
+      mv ${LOG_FILE} ${PWD}/run.log
+      echo "The command failed. The script that was attempted is now available at ${PWD}/run.failed"
+      echo "The log is available at ${PWD}/run.log"
+    fi
+    rm ${TEMP_FILE}
 
   fi
 }
