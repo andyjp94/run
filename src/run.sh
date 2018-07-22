41c7ca4a2e43536bc1e52745f349c6d3e87d337b
Andrew Perry <andrew.perry@cloudreach.com>
2018-07-22 18:12:39 +0100

    ref: split the code up into multiple functions and made it more functional
    Still way too many global variables lying around but can only be reduced
    so far because BASH is not very good at returning variables from
    functions

--- src/run.sh
+++ src/run.sh
@@ -3,42 +3,75 @@
 
 set -o pipefail
 LOCS=("${PWD}/run.json" "${HOME}/run.json" "/etc/run/run.json")
+TEMP_FILE=$(mktemp /tmp/run.XXXXXXXX)
+LOG_FILE=$(mktemp /tmp/run.XXXXXXXX)
 
 function search_file {
+  JSON_CMD=$(cat $1 | jq --arg COMMAND $2 '.commands[] | select(.name == $COMMAND)')
+  if [ "${JSON_CMD}" != "" ]; then
+    CMD="$(echo ${JSON_CMD} | jq -r '.value')"
+    return 0
+  else
+    return 1 
+  fi
+}
 
-  echo "searching $1"
-  a=$(cat $file | jq --arg COMMAND $2 '.commands[] | select(.name == $COMMAND)')
-  if [ "$a" != "" ]; then
-    CMD=$(echo $a | jq -r '.value')
-    num=$(cat $file | jq '.env | length')
-    if [ "$num" != "0" ]; then
+function create_environment { 
+  local RUN_FILE="${1}"
+  local num=$(cat ${1} | jq '.env | length')
+  if [ "$num" != "0" ]; then
     for index in $( seq 1 ${num}); do
-        env_json=$(cat $file | jq  --arg INDEX $((index-1)) '.env[$INDEX |tonumber]')
-        ENV="${ENV} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
+        local env_json=$(cat $RUN_FILE | jq  --arg INDEX $((index-1)) '.env[$INDEX |tonumber]')
+         ENV="${ENV} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
     done
-    fi
+  fi
+  
+}
 
-    TEMP_FILE=$(mktemp /tmp/run.XXXXXXXX)
-    LOG_FILE=$(mktemp /tmp/run.XXXXXXXX)
+function cleanup {
+  rm ${TEMP_FILE}
+  rm ${LOG_FILE}
+}
+
+
+function setup_command {
+    local CMD="${1}"
     echo -e '#!/bin/bash\nrun() {\n '"${CMD}"'\n}\n run' > ${TEMP_FILE}
     chmod +x ${TEMP_FILE}
-    sh -c "${ENV} ${TEMP_FILE}" | tee ${LOG_FILE}
-    if [ "$?" -ne "0" ]; then
-      mv ${TEMP_FILE} ${PWD}/run.failed
-      mv ${LOG_FILE} ${PWD}/run.log
-      echo "The command failed. The script that was attempted is now available at ${PWD}/run.failed"
-      echo "The log is available at ${PWD}/run.log"
-    fi
-    rm ${TEMP_FILE}
+}
 
+function error_handling { 
+  local TEMP_FILE="${1}"
+  local LOG_FILE="${2}"
+  mv ${TEMP_FILE} ${PWD}/run.failed
+  mv ${LOG_FILE} ${PWD}/run.log
+  echo "The command failed. The script that was attempted is now available at ${PWD}/run.failed"
+  echo "The log is available at ${PWD}/run.log"
+  cleanup
+}
+
+function run_command {
+  sh -c "${ENV} ${TEMP_FILE}" | tee "${LOG_FILE}"
+  if [ "$?" -ne "0" ]; then
+    error_handling ${TEMP_FILE} ${LOG_FILE}
+    exit 1
   fi
+}
+function main {
+ for file in ${LOCS[*]}; do 
+    if [ -f $file ]; then
+      if search_file $file $1 ; then
+        create_environment "${file}" 
+        setup_command "${CMD}" "TEMP_FILE" "LOG_FILE"
+        run_command
+
+        cleanup
+        exit 0
+      fi
+    fi
+  done
 }
 
 
 
-
-for file in ${LOCS[*]}; do 
-    if [ -f $file ]; then
-        search_file $file $1
-    fi
-done
+main $1
