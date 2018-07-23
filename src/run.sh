18f9bfd46d10b500cc4e3a2c5738889d05246f3c
Andrew Perry <andrew.perry@cloudreach.com>
2018-07-23 16:05:23 +0100

    feat: make run.sh shellcheck compliant

--- src/run.sh
+++ src/run.sh
@@ -7,9 +7,9 @@ VERSION=0.1
 LOCS=("${PWD}/run.json" "${HOME}/run.json" "/etc/run/run.json")
 
 function find_cmd {
-  JSON_CMD=$(cat $1 | jq --arg COMMAND $2 '.commands[] | select(.name == $COMMAND)')
+  JSON_CMD=$(jq --arg COMMAND "${2}" '.commands[] | select(.name == $COMMAND)' < "${1}")
   if [ "${JSON_CMD}" != "" ]; then
-    CMD="$(echo ${JSON_CMD} | jq -r '.value')"
+    CMD="$(echo "${JSON_CMD}" | jq -r '.value')"
     return 0
   else
     return 1 
@@ -18,23 +18,28 @@ function find_cmd {
 
 function create_environment { 
   local RUN_FILE="${1}"
-  local num=$(cat ${RUN_FILE} | jq '.env | length')
+  local num=0
+  num=$(jq '.env | length' < "${RUN_FILE}")
   if [ "$num" != "0" ]; then
-    for index in $( seq 1 ${num}); do
-        local env_json=$(cat $RUN_FILE | jq  --arg INDEX $((index-1)) '.env[$INDEX |tonumber]')
-         ENV="${ENV} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
+  local env_json=" "
+    for index in $( seq 1 "${num}"); do
+        env_json=$(jq  --arg INDEX $((index-1)) '.env[$INDEX |tonumber]' < "$RUN_FILE")
+        ENV="${ENV} export $(echo "${env_json}" | jq '.name')=$(echo "${env_json}" | jq '.value');"; 
     done
   fi
   
 }
 function create_environment_local {
   local RUN_FILE="${1}"
-  local env_dict=$(cat ${RUN_FILE} | jq --arg COMMAND $2 '.commands[] | select(.name == $COMMAND) |.env')
-  local num=$(echo ${env_dict} | jq 'length')
-  if [ "$num" != "0" ]; then
-    for index in $( seq 1 ${num}); do
-        local env_json=$(echo $env_dict | jq  --arg INDEX $((index-1)) '.[$INDEX |tonumber]')
-         LOCAL_ENV="${LOCAL_ENV} export $(echo $env_json | jq '.name')=$(echo $env_json | jq '.value');"; 
+  local env_dict="{}"
+  env_dict=$(jq --arg COMMAND "${2}" '.commands[] | select(.name == $COMMAND) |.env' < "${RUN_FILE}")
+  local num=0
+  num=$(echo "${env_dict}" | jq 'length')
+  if [ "${num}" != "0" ]; then
+      local env_json=" "
+    for index in $( seq 1 "${num}"); do
+        env_json=$(echo "${env_dict}" | jq  --arg INDEX $((index-1)) '.[$INDEX |tonumber]')
+         LOCAL_ENV="${LOCAL_ENV} export $(echo "${env_json}" | jq '.name')=$(echo "${env_json}" | jq '.value');"; 
     done
   fi
 }
@@ -42,10 +47,13 @@ function create_environment_local {
 function create_path {
   PATH_CMD="export PATH=${PATH}:${PROGDIR}"
   local RUN_FILE="${1}"
-  local num=$(cat ${1} | jq '.path | length')
+  local num=0
+  num=$(jq '.path | length' < "${RUN_FILE}")
+
   if [ "$num" != "0" ]; then
-    for index in $( seq 1 ${num}); do
-        local path_json=$(cat $RUN_FILE | jq  -r --arg INDEX $((index-1)) '.path[$INDEX |tonumber]')
+    local path_json=""
+    for index in $( seq 1 "${num}"); do
+        path_json=$(jq  -r --arg INDEX $((index-1)) '.path[$INDEX |tonumber]' < "${RUN_FILE}")
          PATH_CMD="${PATH_CMD}:${path_json}"
     done
   fi
@@ -53,22 +61,22 @@ function create_path {
 }
 
 function cleanup {
-  rm ${TEMP_FILE}
-  rm ${LOG_FILE}
+  rm "${TEMP_FILE}"
+  rm "${LOG_FILE}"
 }
 
 
 function setup_command {
     local CMD="${1}"
-    echo -e '#!/bin/bash\nrun() {\n '"${CMD}"'\n}\n run' > ${TEMP_FILE}
-    chmod +x ${TEMP_FILE}
+    echo -e '#!/bin/bash\nrun() {\n '"${CMD}"'\n}\n run' > "${TEMP_FILE}"
+    chmod +x "${TEMP_FILE}"
 }
 
 function error_handling { 
   error=$?
   if [ "$error" -ne "0" ]; then
-    mv ${TEMP_FILE} ${PWD}/run.failed
-    mv ${LOG_FILE} ${PWD}/run.log
+    mv "${TEMP_FILE}" "${PWD}"/run.failed
+    mv "${LOG_FILE}" "${PWD}"/run.log
     echo "The command failed. The script that was attempted is now available at ${PWD}/run.failed"
     echo "The log is available at ${PWD}/run.log"
     cleanup
@@ -79,7 +87,7 @@ function error_handling {
 
 function run_command {
   if [ -n "${QUIET}" ];then
-    sh -c "${ENV} ${TEMP_FILE}" 2&> ${LOG_FILE}
+    sh -c "${ENV} ${TEMP_FILE}" 2&> "${LOG_FILE}"
   else
     sh -c "${ENV} ${TEMP_FILE}" | tee "${LOG_FILE}"
   fi
@@ -87,16 +95,14 @@ function run_command {
 }
 function main {
  for file in ${LOCS[*]}; do 
-    if [ -f $file ]; then
-      if find_cmd $file $1 ; then
+    if [ -f "${file}" ]; then
+      if find_cmd "$file" "${1}" ; then
         create_environment "${file}"
-        create_environment_local "$file" $1
+        create_environment_local "$file" "${1}"
         create_path "${file}" 
         setup_command "${CMD}" "TEMP_FILE" "LOG_FILE"
         ENV="${ENV}${LOCAL_ENV}${CLI_ENV}${PATH_CMD}"
         run_command
-
-        # cleanup
         return 0
       fi
     fi
@@ -106,26 +112,27 @@ function main {
 function list_commands {
   
  for file in ${LOCS[*]}; do 
-    if [ -f $file ]; then
+    if [ -f "${file}" ]; then
      echo "Commands available in ${file}:"
-     cat $file | jq  '.commands'
+     jq  '.commands' < "${file}"
     fi
 
     done
 }
 
 function complete_commands {
-  
+  local FILE_COMMANDS=""
+  local COMMANDS=""
  for file in ${LOCS[*]}; do 
-    if [ -f $file ]; then
-     local FILE_COMMANDS=$(cat ${file} | jq  -r '.commands[] | .name' )
-     local COMMANDS="${COMMANDS} ${FILE_COMMANDS}"
+    if [ -f "${file}" ]; then
+     FILE_COMMANDS=$(jq  -r '.commands[] | .name' < "${file}" )
+     COMMANDS="${COMMANDS} ${FILE_COMMANDS}"
     fi
     done
     COMMANDS=$(echo "${COMMANDS}" | xargs -n 1 | sort -u )
 
     COMP_FILE="${HOME}/run_completions.sh"
-    echo "complete -W ${COMMANDS} run.sh" >  ${COMP_FILE}
+    echo "complete -W ${COMMANDS} run.sh" >  "${COMP_FILE}"
     echo "The autocomplete configuration files is available at ${COMP_FILE}."
     echo "To use it simply source it, to source it on login add:"
     echo "source ${COMP_FILE}"
@@ -154,7 +161,7 @@ usage() {
   echo "      Sends stderr and stdout to the log file alone."
   echo
 	echo "  -l, --list <file>"
-	echo "      List the available commands, these are gathered from: ${LOCS[@]}"
+	echo "      List the available commands, these are gathered from: ${LOCS[*]}"
 	echo
 	echo "  -v, --version"
 	echo "      Prints the version of the command."
@@ -164,13 +171,13 @@ usage() {
 	echo
 }
 # File name
-readonly PROGNAME=$(basename $0)
+readonly PROGNAME=$(basename "${0}")
 # File name, without the extension
 readonly PROGBASENAME=${PROGNAME%.*}
 # File directory
 readonly PROGDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Arguments
-readonly ARGS="$@"
+readonly ARGS=("$@")
 # Arguments number
 readonly ARGNUM="$#"
 
@@ -214,40 +221,40 @@ do
 	# an option argument, continue
 	*)	;;
 	esac
-  if ! [[ $string = *"-"* ]]; then
-    return 0
-  fi
+          if ! [[ "${1}" = *"-"* ]]; then
+            return 0
+          fi
 	shift
 done
 
-echo $1
 
-if [ -z $1 ]; then
+if [ -z "${1}" ]; then
   echo "Must specify the command."
   exit 1
  fi
 }
 
-parse_arguments $@
+parse_arguments "${@}"
 TEMP_FILE=$(mktemp /tmp/run.XXXXXXXX)
 LOG_FILE=$(mktemp /tmp/run.XXXXXXXX)
 trap error_handling EXIT
 
 
 if [[ $1 = *","* ]]; then
-  IFS=', ' read -r -a array <<< $1
+  IFS=', ' read -r -a array <<< "${1}"
   for element in "${array[@]}"
   do
-      main $element
+      main "${element}"
   done
 
 else
   while [ "$#" -gt 0 ]
     do
-    main $1
+    main "${1}"
     shift 
     done
 fi
+
 
 
 
